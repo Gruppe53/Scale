f2dbda3cd2b5f1d03b6598baf0113f4148212b5d
Kristin Hansen <maagenator@gmail.com>
2014-03-03 14:37:18 +0100

    ...

---
+++ 55_CDIO2.1/src/run/Main.java
@@ -0,0 +1,89 @@
+package run;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import scale.*;
+import users.*;
+import simulator.*;
+import net.miginfocom.swing.MigLayout;
+
+public class Main extends JComponent {
+	private static final long serialVersionUID = 1L;
+	private static JFrame scaleFrame;
+	private static JComponent newContentPane;
+	
+	private JTabbedPane tab;
+	
+	private IScaleConnection con;
+	private IScale scale;
+	private ScaleDialog sDialog;
+
+	private IOperatorDTO oprDTO;
+	private IOperatorDAO oprDAO;
+	private UserDialog  uDialog;
+	
+	private ISimulatorConnection simCon;
+	private ISimulator sim;
+	private SimulatorDialog simDialog;
+	
+	public Main() {
+		// Set layout manager
+		setLayout(new MigLayout());
+		
+		// Create tabbed panel
+		UIManager.put("TabbedPane.tabAreaBackground", Color.decode("#f0f0f0"));
+		UIManager.put("TabbedPane.focus", Color.decode("#c8ddf2"));
+		tab = new JTabbedPane();
+		tab.setOpaque(true);
+		
+		// Instantiate objects
+		con = new ScaleConnection();
+		scale = new Scale(con);
+		sDialog = new ScaleDialog(scale);
+		
+		oprDTO = new OperatorDTO();
+		oprDAO = new OperatorDAO(oprDTO);
+		uDialog = new UserDialog(oprDAO, tab);
+		
+		simCon = new SimulatorConnection();
+		sim = new Simulator();
+		simDialog = new SimulatorDialog(sim);
+		
+		tab.addTab("Userpanel", uDialog);
+		tab.addTab("Scale Console", sDialog);
+		tab.addTab("Simulator", simDialog);
+		
+		tab.setEnabledAt(1, false);
+		
+		add(tab);
+	}
+	
+	private static void createAndShowGUI(String name) {
+        // Create window
+		scaleFrame = new JFrame(name);
+		scaleFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		scaleFrame.setBackground(Color.decode("#f0f0f0"));
+		scaleFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("materials/icon.png"));
+		scaleFrame.setResizable(false);
+
+        // Create the content pane
+        newContentPane = new Main();
+        newContentPane.setOpaque(true);
+        scaleFrame.setContentPane(newContentPane);
+
+        // Draw the window
+        scaleFrame.setLocationRelativeTo(null);
+        scaleFrame.pack();
+        scaleFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI("Scale Console"); // Name of window
+            }
+        });
+    }
+}
---
+++ 55_CDIO2.1/src/simulator/SimulatorDialog.java
@@ -0,0 +1,45 @@
+package simulator;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.*;
+
+import net.miginfocom.swing.MigLayout;
+
+public class SimulatorDialog extends JComponent {
+	private JPanel simPanel = new JPanel(new MigLayout());
+	private JTextArea textArea = new JTextArea();
+	private JScrollPane txtPanel = new JScrollPane(textArea);
+	
+	private ISimulator sim;
+	
+	public SimulatorDialog(ISimulator sim) {
+		this.sim = sim;
+		
+		setLayout(new MigLayout());
+		
+		simPanel.setBorder(BorderFactory.createBevelBorder(1, Color.decode("#ffffff"), Color.decode("#898c95"), Color.decode("#898c95"), Color.decode("#f0f0f0")));
+		simPanel.setBackground(Color.WHITE);
+		
+		txtPanel.setBackground(Color.white);
+		txtPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.decode("#d5dfe5")), "Console"));
+		txtPanel.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		txtPanel.setPreferredSize(new Dimension(400, 200));
+		
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		textArea.setEditable(false);
+		textArea.append("[" + getDate() + "]\tPress start to run Scale Simulator.\n");
+		
+		simPanel.add(txtPanel);
+		
+		add(simPanel);
+	}
+	
+	private String getDate() {
+		return new SimpleDateFormat("HH:mm:ss").format(new Date());
+	}
+}
